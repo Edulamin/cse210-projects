4de0ff50977d46b16c75ac0e4c646e03a4df977b
Edulamin <edulamin10@gmail.com>
2023-09-28 18:52:52 -0300

    Prep 3 submition

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -3,7 +3,40 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep3 World!");
+    {   
+        
+        string repeat = "yes";
+
+        while (repeat == "yes")
+        {          
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+        int numberGuess = -1;
+        int guessCount = 0;
+
+            while (numberGuess != magicNumber)
+            {
+                Console.Write("Guess the magic number: ");
+                numberGuess = int.Parse(Console.ReadLine());
+                guessCount ++;
+                
+                if (numberGuess < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (numberGuess > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }                
+            else
+            {
+                Console.WriteLine("You guessed the number");
+                Console.WriteLine($"It took {guessCount} guesses!");
+            }
+
+            }
+            Console.Write("Do you want to play again? ");
+            repeat = Console.ReadLine();
+        }  
     }
 }
